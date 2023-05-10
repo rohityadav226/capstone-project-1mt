f2a2a4ee735f7697f1bd31a20d44cec6c8c6fce4
Rohit Yadav <90460563+rohityadav226@users.noreply.github.com>
2023-05-10 09:57:52 -0400

    Update html_css.py

--- app/html_css.py
+++ app/html_css.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 class design():
     def heading_and_title():
-        st.set_page_config(page_title="1millionteachers", page_icon="📈", layout="wide")
+        st.set_page_config(page_title="NAME REMOVED FOR PRIVACY CONCERNS", page_icon="📈", layout="wide")
         html_temp = """
             <div style="background-color:rgb(215,78,91);padding:10px">
             <h2 style="color:white;text-align:center;">Sentiment Analysis Dashboard</h2>
@@ -11,7 +11,7 @@ class design():
         st.markdown(html_temp,unsafe_allow_html=True)
 
     def add_logo():
-        logo_url = 'https://www.1millionteachers.com/wp-content/uploads/2020/02/logo.png'
+        logo_url = 'URL REMOVED FOR PRIVACY CONCERNS'
         st.sidebar.image(logo_url, width=200)
 
     def sidebar_content():
